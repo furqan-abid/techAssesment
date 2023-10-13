847d3a5c1b4e1b0e2f5e011bf59672629ebbbb72
furqan-abid <furqanabid10@gmail.com>
2023-10-13 16:44:03 +0500

    api slice updated

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -4,14 +4,7 @@ import {createApi , fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath:'api',
     baseQuery:fetchBaseQuery({
-        baseUrl:'http://localhost:4000/api/',
-        prepareHeaders:(headers)=>{
-            const token = `Token ${localStorage.getItem('token')}`
-            if(token){
-                headers.set('authorization',`${token}`)
-            }
-            return headers
-        },
+        baseUrl:'http://localhost:4000/api/'
     }),
     tagTypes:['Cart'],
     endpoints:builder=>({})
